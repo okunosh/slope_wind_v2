f86415023ebafdc66ca939d137713f9b13d3e129
okuno <g2050230@cc.kyoto-su.ac.jp>
2025-07-09 16:08:12 +0900

    testcase imitationg zardi2015 fig1

---
+++ analytical/params/testcase.py
@@ -0,0 +1,16 @@
+#Zardi2015 Fig1
+
+params = {
+    "planet": "Earth",
+    "alpha_deg": 1.0,
+    "Theta": 5,
+    "theta_0": 288,
+    "gamma": 0.003,
+    "K": 3.0,
+    "psi": 0.0,
+    "tmax": 3600*24,    # 1時間
+    "zmax": 300,     # 200m
+    "dt": 3600*3,       # 3h刻み
+    "dz": 20,        # 20m刻み
+    "output": "/home/okuno/2026M_okuno/slopeWind_v2/output/initial_test"
+}
